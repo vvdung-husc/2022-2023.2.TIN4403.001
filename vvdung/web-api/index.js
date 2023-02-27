31487e327719bf34c291b98da5e3444e4cc7ede3
Vo Viet Dung <vvdung@hueuni.edu.vn>
2023-02-27 14:49:16 +0700

    vvdung

--- vvdung/web-api/index.js
+++ vvdung/web-api/index.js
@@ -7,11 +7,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //console.log("HELLO NODEJS")
-
+//sử dụng để kiểm tra API có đang hoạt động
 app.get("/", function (req, res) {
   res.status(200).send("Welcome to RESTFUL API - NODEJS - TINK43");
 });
 
+//hàm đăng nhập - nhận thông tin tài khoản từ Android App
 app.post("/login", function (req, res) {
   var user = req.body.username;
   var pass = req.body.password;	
@@ -22,13 +23,18 @@ app.post("/login", function (req, res) {
   else
     res.status(503).send("API LOGIN - LOI TAI KHOAN");
 });
+
+//hàm đăng ký tài khoản
 app.post("/register", function (req, res) {
   res.status(200).send("API REGISTER - POST");
 });
+
+//hàm nhận thông tin tài khoản sau khi đã đăng nhập thành công
 app.post("/userinfo", function (req, res) {
   res.status(200).send("API USERINFO - POST");
 });
 
+//dịch vụ WebService chạy tại cổng số n
 var server = app.listen(4380, function () {
   console.log("app running on port.", server.address().port);
 });  
