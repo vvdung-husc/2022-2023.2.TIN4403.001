73319e2785658102095815b7a9d54ffacfb7d819
huyle55 <ledinhngochuy55@gmail.com>
2023-03-02 09:12:11 +0700

    ldnhuy

--- LDNHuy-husc/app/src/main/java/com/example/myapplaptrinhdidong1/MainActivity.java
+++ LDNHuy-husc/app/src/main/java/com/example/myapplaptrinhdidong1/MainActivity.java
@@ -1,28 +1,39 @@
-package com.example.testing;
+package com.example.myapplaptrinhdidong1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.os.Bundle;
+
 import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    static String   _userNameLogined;
     EditText m_edtUser,m_edtPass; //Biến điều khiển EditText
-    Button m_btnLogin; //Biến điều khiển Button
+    Button m_btnLogin, m_btnRegister; //Biến điều khiển Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //Khởi tạo các biến điều khiển tương ứng trong layout
         m_edtUser = (EditText)findViewById(R.id.edtUser);
         m_edtPass = (EditText)findViewById(R.id.edtPassword);
         m_btnLogin = (Button) findViewById(R.id.btnLogin);
+        m_btnRegister = (Button)findViewById(R.id.btnRegister);
+
         //Cài đặt sự kiện Click cho Button Login
         m_btnLogin.setOnClickListener(new CButtonLogin());
+        //Cài đặt sự kiện Click cho Button Register
+        m_btnRegister.setOnClickListener(new CButtonRegister());
+
     }
+
     public class CButtonLogin  implements View.OnClickListener {
         @Override
         public void onClick(View v) {//Hàm sử lý sự kiện click button login
@@ -46,8 +57,33 @@ public class MainActivity extends AppCompatActivity {
 
     //Hàm dịch vụ Login
     void apiLogin(String user, String pass) throws IOException {
-        String json = "{\"username\":\"" + user + "\",\"password\":\"" + pass +"\"}";
+        boolean bOk = (user.equals("vvdung") && pass.equals("123456"));
+        String json = "{\"username\":\"" + user + "\",\"password\":\"" + pass +"\"}" + bOk;
         Toast.makeText(getApplicationContext(),json,Toast.LENGTH_SHORT).show();
         Log.d("K43",json);
+
+        if (bOk){
+            _userNameLogined = "Võ Việt Dũng";
+            Intent intent = new Intent(getApplicationContext(),UserActivity.class);
+            startActivity(intent);
+        }
+        else{
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu không chính xác.",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public class CButtonRegister implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {//Hàm sử lý sự kiện click button register
+            //Toast.makeText(getApplicationContext(),"CButtonRegister::onClick...",Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(i);
+        }
     }
 }
