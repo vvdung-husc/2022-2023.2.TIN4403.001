27f9cd7f9538411cd98922b2f53d885a0383b6a1
huyle55 <ledinhngochuy55@gmail.com>
2023-03-06 22:44:38 +0700

    huytest

--- LDNHuy-husc/laptrinhdidong/app/src/main/java/com/example/myapplaptrinhdidong1/MainActivity.java
+++ LDNHuy-husc/laptrinhdidong/app/src/main/java/com/example/myapplaptrinhdidong1/MainActivity.java
@@ -12,7 +12,18 @@ import android.widget.Toast;
 
 import java.io.IOException;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     static String   _userNameLogined;
     EditText m_edtUser,m_edtPass; //Biến điều khiển EditText
     Button m_btnLogin, m_btnRegister; //Biến điều khiển Button
@@ -40,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             String user = m_edtUser.getText().toString();
             String pass = m_edtPass.getText().toString();
             Log.d("K43","CLICK BUTTON LOGIN ACCOUNT " + user + "/" + pass);
-            if (user.length() <= 3 || pass.length() < 6){
+            if (user.length() < 3 || pass.length() < 6){
                 Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu không hợp lệ!",Toast.LENGTH_SHORT).show();
                 return;
             }
@@ -55,15 +66,106 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class CButtonRegister implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {//Hàm sử lý sự kiện click button register
+            //Toast.makeText(getApplicationContext(),"CButtonRegister::onClick...",Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(i);
+        }
+    }
+
+    //Hàm mẫu sử dụng phương thức GET
+    void doGet(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //txtString.setText(myResponse);
+                        Log.d("K43",myResponse);
+                    }
+                });
+            }
+        });
+    }
+
+    //Hàm mẫu sử dụng phương thức POST
+    void doPost(String url,String json) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON,json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("K43",response.body().string());
+            }
+        });
+    }
     //Hàm dịch vụ Login
     void apiLogin(String user, String pass) throws IOException {
-        boolean bOk = (user.equals("ldnghuy") && pass.equals("123456"));
-        String json = "{\"username\":\"" + user + "\",\"password\":\"" + pass +"\"}" + bOk;
+        //boolean bOk = (user.equals("vvdung") && pass.equals("123456"));
+        String json = "{\"username\":\"" + user + "\",\"password\":\"" + pass +"\"}";
         Toast.makeText(getApplicationContext(),json,Toast.LENGTH_SHORT).show();
         Log.d("K43",json);
 
-        if (bOk){
-            _userNameLogined = "Lê Đình Ngọc Huy";
+        RequestBody body = RequestBody.create(JSON,json);
+        Request request = new Request.Builder()
+                .url("http://192.168.56.1:4380/login")
+                .post(body)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String errStr = "Tài khoản hoặc mật khẩu không chính xác.\n" + e.getMessage();
+                Log.d("K43","onFailure\n" + errStr);
+                Toast.makeText(getApplicationContext(),errStr,Toast.LENGTH_SHORT).show();
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String errStr = "Tài khoản hoặc mật khẩu không chính xác.\n" + response.body().string();
+                Log.d("K43",errStr);
+                if (!response.isSuccessful()){
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),errStr,Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    return;
+                }
+
+                Intent intent = new Intent(getApplicationContext(),UserActivity.class);
+                startActivity(intent);
+
+            }
+        });//client.newCall(request).enqueue(new Callback() {
+
+        /*if (bOk){
+            _userNameLogined = "Võ Việt Dũng";
             Intent intent = new Intent(getApplicationContext(),UserActivity.class);
             startActivity(intent);
         }
@@ -74,16 +176,8 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu không chính xác.",Toast.LENGTH_SHORT).show();
                 }
             });
-        }
+        }*/
     }
 
-    public class CButtonRegister implements View.OnClickListener {
 
-        @Override
-        public void onClick(View v) {//Hàm sử lý sự kiện click button register
-            //Toast.makeText(getApplicationContext(),"CButtonRegister::onClick...",Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-            startActivity(i);
-        }
-    }
 }
