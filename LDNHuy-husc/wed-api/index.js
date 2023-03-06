e38fe5ac3890af57ce9fb6f5490b788c7ca6dfd8
huyle55 <ledinhngochuy55@gmail.com>
2023-03-06 21:56:24 +0700

    ngochuy

--- LDNHuy-husc/wed-api/index.js
+++ LDNHuy-husc/wed-api/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var async   = require('async');
+var LTDB    = require('./_database/db_ltdd');
+var USER    = require('./models/user');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -17,11 +21,12 @@ app.post("/login", function (req, res) {
   var user = req.body.username;
   var pass = req.body.password;	
   console.log("ACCOUNT:",user, "/",pass );
+  login(user,pass,res);
 
-  if (user == "vvdung" && pass == "13572468" )
-    res.status(200).send("API LOGIN - THANH CONG");
-  else
-    res.status(503).send("API LOGIN - LOI TAI KHOAN");
+  // if (user == "vvdung" && pass == "13572468" )
+  //   res.status(200).send("API LOGIN - THANH CONG");
+  // else
+  //   res.status(503).send("API LOGIN - LOI TAI KHOAN");
 });
 
 //hàm đăng ký tài khoản
@@ -34,7 +39,45 @@ app.post("/userinfo", function (req, res) {
   res.status(200).send("API USERINFO - POST");
 });
 
-//dịch vụ WebService chạy tại cổng số n
-var server = app.listen(4380, function () {
-  console.log("app running on port.", server.address().port);
-});  
+
+initDatabase(function (err,result){
+  if (err) process.exit(1);
+  //dịch vụ WebService chạy tại cổng số n
+  var server = app.listen(4380, function () {
+      console.log("app running on port.", server.address().port);
+  });    
+});
+
+function initDatabase(callback) {
+  async.waterfall([
+    function (next) {
+      LTDB.Init(next);
+    }
+  ], callback);
+}
+//hàm đăng nhập - kiểm tra trong database
+function login(user,pass,res){
+  var oDataRet = [];
+  async.waterfall([
+    function (next) {
+      USER.Authentication(user,pass,next);
+    },    
+    function (oUser,next) {
+      oDataRet = oUser;
+      next(null,oDataRet);
+    }
+  ],function (err, result) {
+    if (err) {
+      var oResult = {};
+      oResult.r = -1;
+      oResult.m = result;
+      return res.status(302).json(oResult);      
+    }
+    else {
+      var oResult = {};
+      oResult.r = 0;
+      oResult.m = oDataRet;
+      return res.status(200).json(oResult);
+    }
+  });   
+}
