ddb59ae1e1eb465ec7ce10b98b542716c74a6973
huyle55 <ledinhngochuy55@gmail.com>
2023-03-23 08:13:50 +0700

    ngoc huy

--- LDNHuy-husc/web-api/_database/db_ltdd.js
+++ LDNHuy-husc/web-api/_database/db_ltdd.js
@@ -14,8 +14,8 @@ CDBLTDD.prototype.Init = function (callback) {
     console.log('Connecting LTDD Database...');
     async.waterfall([
       function (next) {
-        // fnConnectDatabase('mongodb://testuser:ltdd%40123@222.255.148.165:9017/LTDD',next);
-        fnConnectDatabase('mongodb://127.0.0.1:27017/LTDD',next);        
+        fnConnectDatabase('mongodb://testuser:ltdd%40123@222.255.148.165:9017/LTDD',next);
+        // fnConnectDatabase('mongodb://127.0.0.1:27017/LTDD',next);        
       },
       function (next) {
         USERS      = require('../models/user');                
